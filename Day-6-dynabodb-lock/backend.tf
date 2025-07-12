0d4169f28d6465bcf33b05879e6b10e65fc6d11b
jayu2000 <patiljm017@gmail.com>
2025-07-12 23:41:20 +0530

    old new version of state lock

--- Day-6-dynabodb-lock/backend.tf
+++ Day-6-dynabodb-lock/backend.tf
@@ -9,3 +9,18 @@ terraform {
     
   }
 }
+
+
+#there is two type of lock file s3+dynomodb is old version and only s3 is new version
+
+#dynomodblock-old version :-
+#Below are command for old version:-
+# dynamodb_table = "terradynamodb"
+# encrypt = true
+
+#below command for new version
+#use_lockfile = true 
+
+#due to lock feature -two developer not able to do apply at same time
+#as getting error ("error aquiring the state lock")
+#as in code i mentioned backend "s3" its mean statefile automatically store into s3 bucket for our safety
