b9673cde2ec329a8c8685b5d033747291d14a7ac
jayu2000 <patiljm017@gmail.com>
2025-07-12 22:50:25 +0530

    dynamodb-lock

--- Day-6-dynabodb-lock/backend.tf
+++ Day-6-dynabodb-lock/backend.tf
@@ -1,7 +1,10 @@
 terraform {
   backend "s3" {
     bucket = "samjayu-bucket"
-    key    = "terraform.tfstate"
+    key = "terraform.tfstate"
+    dynamodb_table = "terradynamodb"
     region = "us-east-1"
+    encrypt = true
+    
   }
 }
